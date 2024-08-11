6a59150958d020475a6481b5635b14ca617a8e2a
unknown <dikfajar06@gmail.com>
2024-08-11 21:14:47 +0700

    fix (home) : pass foto

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -21,7 +21,7 @@
         </div> --}}
     <div id="home" class="hero bg-base-200 min-h-screen">
         <div class="hero-content flex-col lg:flex-row-reverse">
-            <img src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.webp"
+            <img src="{{ asset('assets/images/pass_foto.jpg') }}"
                 class="max-w-sm rounded-lg shadow-2xl" />
             <div>
                 <h1 class="text-5xl font-bold">Dikdik Fajar Permadi</h1>
